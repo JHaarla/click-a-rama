9d3e360e4597380576463a88f8727abfb0828574
JHaarla <haarladesign@gmail.com>
2020-03-10 23:56:17 -0400

    adding basic character card

---
+++ src/components/CharCard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CharCard.css";
+
+const CharCard = (props) => (
+    <div className="card">
+        <div className="img-container">
+            <img className="img-thumbnail" alt={props.name} src={props.imglink} />
+        </div>
+    </div>
+)
+
+export default CharCard;
