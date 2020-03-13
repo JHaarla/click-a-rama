b27cd35d24199da8310c3b7646bca908aff2da10
JHaarla <haarladesign@gmail.com>
2020-03-13 00:08:52 -0400

    adding info to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,9 @@ import "./header.css";
 
 const Header = props => (
     <div className="header">
-    
+        <div className="title">Click-A-Rama</div>
+        <div className="mesg">{props.message}</div>
+        <div className="score">Score: {props.score}  |  High Score: {props.highscore}</div>
     </div>
 );
 
