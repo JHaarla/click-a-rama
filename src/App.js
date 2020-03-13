f0133b5ab4bb700f5861b5438cf984d5bfefaae7
JHaarla <haarladesign@gmail.com>
2020-03-12 22:52:56 -0400

    added card wrapper so we can force a grid and styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CharCard from "./components/CharCard";
 import characters from "./futurama.json";
+import CardWrapper from "./components/CardWrapper";
 
 // class App extends Component {
 
@@ -15,15 +16,18 @@ function App() {
     return (
     <>
       <Header />
-      {characters.map(img => (
 
-      
+      <CardWrapper>
+            {characters.map(img => (
+
       <CharCard 
         id={img.id}
         key={img.id}
         imglink={img.imglink}
         />
+        
         ))}
+        </CardWrapper>
       <Footer />
       </>
     )
---
+++ src/components/CardWrapper/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CardWrapper(props) {
+    return (
+        <div className="card-wrapper">{props.children}</div>
+    )
+}
+
+export default CardWrapper;
