af86800032287f823162c4389e6efb8e667297f7
JHaarla <haarladesign@gmail.com>
2020-03-14 21:36:45 -0400

    changed app.js to a smart component so we can handle state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 // import Component from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -6,35 +6,41 @@ import CharCard from "./components/CharCard";
 import characters from "./futurama.json";
 import CardWrapper from "./components/CardWrapper";
 
-// class App extends Component {
+class App extends Component {
 
-function App() {
+  // function App() {
+  state = {
+    characters,
+    score: 0,
+    highscore: 0,
+    clickedArray: [],
+    message: ""
+  }
 
 
-
-  // render () {
+  render() {
     return (
-    <>
-      <Header />
+      <>
+        <Header />
 
-      <CardWrapper>
-            {characters.map(img => (
+        <CardWrapper>
+          {characters.map(img => (
 
-      <CharCard 
-        id={img.id}
-        key={img.id}
-        imglink={img.imglink}
-        />
-        
-        ))}
+            <CharCard
+              id={img.id}
+              key={img.id}
+              imglink={img.imglink}
+            />
+
+          ))}
         </CardWrapper>
-      <Footer />
+        <Footer />
       </>
     )
   }
 
 
 
-// }
+}
 
 export default App;
