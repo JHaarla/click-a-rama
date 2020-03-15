a0cab1e97969ef11147528f62cf00a2fd38f0929
JHaarla <haarladesign@gmail.com>
2020-03-15 00:06:39 -0400

    tweaking styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import "./header.css";
 
 const Header = props => (
     <div className="header">
-        <div className="title">Click-A-Rama</div>
-        <div className="mesg">{props.message}</div>
+        <div className="title">Click-A-Rama<span className="headerSpan"> (A React Game)</span></div>
+        {/* <div className="mesg">{props.message}</div> */}
         <div className="score">Score: {props.score}  |  High Score: {props.highScore}</div>
     </div>
 );
