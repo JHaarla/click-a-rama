fca961d0f9007deca37103f4b866df2c1be77bf7
JHaarla <haarladesign@gmail.com>
2020-03-15 01:19:47 -0400

    minor css tweaks. This is more or less done!

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import "./footer.css";
 
 function Footer() {
     return (
-        <div className="footer">© 2020 Jarkko Haarla</div>
+        <div className="footer text-center">© 2020 Jarkko Haarla</div>
     )
 };
 
