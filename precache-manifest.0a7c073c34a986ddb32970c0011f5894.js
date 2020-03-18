fededdd87e2d84afb7febb5eba053dd5c502eb9e
JHaarla <haarladesign@gmail.com>
2020-03-17 23:11:54 -0400

    Updates

---
+++ precache-manifest.0a7c073c34a986ddb32970c0011f5894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c10096d36f76931b26d17adddc8ff88",
+    "url": "/Click-A-Rama/index.html"
+  },
+  {
+    "revision": "babf2a1e7ff36bb2b973",
+    "url": "/Click-A-Rama/static/css/main.bc010651.chunk.css"
+  },
+  {
+    "revision": "5f67f69517bab6bddaba",
+    "url": "/Click-A-Rama/static/js/2.1e42d091.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Click-A-Rama/static/js/2.1e42d091.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babf2a1e7ff36bb2b973",
+    "url": "/Click-A-Rama/static/js/main.e2f841a8.chunk.js"
+  },
+  {
+    "revision": "070ad5c9b23a925d2029",
+    "url": "/Click-A-Rama/static/js/runtime-main.ae89bc70.js"
+  }
+]);
